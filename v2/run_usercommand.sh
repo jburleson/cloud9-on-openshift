2bc4d00c5524f8d9fc156f6f926dca1d8bec9e1e
Capuccino <chinodesuuu@gmail.com>
2018-07-24 03:36:00 +0000

    fix c9 not finding this shit

--- v2/run_usercommand.sh
+++ v2/run_usercommand.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-bash -c "
-/opt/bin/cloud9.sh \ 
-   -w /workspace 
-   -l 0.0.0.0 
-   -p 8181 
-   --username $C9_USERNAME 
-   --password $C9_PASSWORD
-"
+cd /opt/cloud9 && \
+bash bin/cloud9.sh \ 
+     -w /workspace 
+     -l 0.0.0.0 
+     -p 8181 
+     --username $C9_USERNAME 
+     --password $C9_PASSWORD;
