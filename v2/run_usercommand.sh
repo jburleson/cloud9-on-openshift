25255dacd6e37ded947f03d76792e20064ad810a
Capuccino <chinodesuuu@gmail.com>
2018-07-25 07:38:15 +0000

    redo this

--- v2/run_usercommand.sh
+++ v2/run_usercommand.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 ls cloud9
-node cloud9/server.js
-     -w /workspace 
-     -l 0.0.0.0 
-     -p 8181 
-     --username $C9_USERNAME 
-     --password $C9_PASSWORD;
+node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $C9_USERNAME --password $C9_PASSWORD;
