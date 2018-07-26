a52f92b9f7115a28221bc86df63ff461acb5e81b
Capuccino <enra@headbow.stream>
2018-07-26 14:50:19 +0800

    fix(shell): fix ambigous line.

--- v2/run_usercommand.sh
+++ v2/run_usercommand.sh
@@ -4,6 +4,6 @@ if [-z "$C9_PASSWORD" ] && [-z "$C9_USERNAME"]; then
    echo "Starting Cloud9..."
    node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181 --username $C9_USERNAME --password $C9_PASSWORD;
 else
-   echo "[warn] Starting Cloud9 Without Authorization is highly discouraged. Set them from the C9_USERNAME and C9_USERNAME variables!"
+   echo "[warn] Starting Cloud9 Without Authorization is highly discouraged. Set them from the C9_USERNAME and C9_PASSWORD variables!"
    node cloud9/server.js -w /workspace -l 0.0.0.0 -p 8181;
 fi
