f74c307fe03b07f450931adb7bbf5e78b3178ffd
Capuccino <chinodesuuu@gmail.com>
2018-08-18 15:48:38 +0800

    Some parts were missing so lemme fix a bit

--- v3/run_container.sh
+++ v3/run_container.sh
@@ -28,14 +28,15 @@ dnf -y install \
 wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo && \
      dnf -y update && \
      dnf -y install cf-cli
+     
 # Create user
 adduser user -u 1000 -g 0 -r -m -d /home/user/ -c "Default Application User" -l
 echo "user ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/user
 chmod 0440 /etc/sudoers.d/user
 
 # Ansible deployment
- curl -sSL https://github.com/gbraad/ansible-playbooks/raw/master/playbooks/install-c9sdk.yml -o /tmp/install.yml
- su - user -c "ansible-playbook /tmp/install.yml"
+curl -sSL https://github.com/gbraad/ansible-playbooks/raw/master/playbooks/install-c9sdk.yml -o /tmp/install.yml
+su - user -c "ansible-playbook /tmp/install.yml"
 
 # Allow user installs in /opt as root
 chmod g+rw /opt
@@ -43,8 +44,7 @@ chgrp root /opt
 
 # Create work directory
 mkdir -p /workspace
-chown -R user:root /workspace
-chmod -R g+rw /workspace
+chown user:root /workspace
 
 # allow to run on openshift
 chown -R user:root /opt/c9sdk
@@ -54,3 +54,4 @@ find /home/user -type d -exec chmod g+x {} +
 
 # Clean up
 dnf clean all
+rm /tmp/install.yml
