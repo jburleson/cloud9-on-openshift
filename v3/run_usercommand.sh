5208ffc8eba46a68f84f6ee0f7df046645af38b9
Capuccino <chinodesuuu@gmail.com>
2018-08-18 14:43:30 +0800

    Fix Entrypoint error

--- v3/run_usercommand.sh
+++ v3/run_usercommand.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/home/user/.c9/node/bin/node /opt/app/server.js \
+/home/user/.c9/node/bin/node /opt/c9sdk/server.js \
     --listen 0.0.0.0 \
     --port 8181 \
     -a $C9_USERNAME:$C9_PASSWORD \
