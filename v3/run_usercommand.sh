c82f84660458cc73d1bf54251ce40fd590d4975c
Capuccino <chinodesuuu@gmail.com>
2018-08-23 07:29:33 +0000

    revert things to known revision in f27

--- v3/run_usercommand.sh
+++ v3/run_usercommand.sh
@@ -9,5 +9,5 @@ export NSS_WRAPPER_GROUP=/etc/group
 /home/user/.c9/node/bin/node /opt/c9sdk/server.js \
     --listen 0.0.0.0 \
     --port 8181 \
-    -a $USERNAME:$PASSWORD \
+    -a $C9_USERNAME:$C9_PASSWORD \
     -w /workspace
